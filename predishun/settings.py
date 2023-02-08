e497eeb536a4104a184abfde0816730033bd7e5a
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-02-08 13:47:59 +0100

    extend access token lifetime

--- predishun/settings.py
+++ predishun/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import environ
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,6 +78,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=25),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 ROOT_URLCONF = 'predishun.urls'
