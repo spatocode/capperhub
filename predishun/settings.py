7ecfce1b5a3dafe078af19708ad1ba8bc7c0e99f
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-06-17 15:05:19 +0100

    update container configurations

--- predishun/settings.py
+++ predishun/settings.py
@@ -346,8 +346,8 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 else:
     # aws settings
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
