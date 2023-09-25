33f2556f470d2622945ec3ba2ab2538db8f88d91
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-09-25 13:29:45 +0100

    fix: reconfigure rate-limit middleware to get correct ip

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,5 +1,11 @@
 def reverse_proxy(get_response):
     def process_request(request):
-        request.META['REMOTE_ADDR'] = request.META.get('X-Forwarded-For') or request.META.get('REMOTE_ADDR')
+        xff = request.META.get('X-Forwarded-For')
+        raddr = request.META.get('REMOTE_ADDR')
+        if "X-Forwarded-For" in request.META:
+            xff = request.META.get('X-Forwarded-For').split(",", 1)[0]
+        if "REMOTE_ADDR" in request.META:
+            raddr = request.META.get('REMOTE_ADDR').split(",", 1)[0]
+        request.META['REMOTE_ADDR'] = xff or raddr
         return get_response(request)
     return process_request
